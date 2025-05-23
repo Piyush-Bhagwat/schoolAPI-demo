4ddba7b30bcba4ef30dc6063db094f3516b3cf6f
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2025-05-23 14:51:09 +0530

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 require("./db");
 const schoolRoutes = require("./routes/schoolRoutes");
 
 app.use(express.json());
+app.use(cors());
 app.use("/", schoolRoutes);
 
 const PORT = process.env.PORT || 3000;
